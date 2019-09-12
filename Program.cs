8fed141c7016c1e669ab1115f15ce1159f86a390
edson arthur <edson005@gmail.com>
2019-09-11 22:20:52 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GameTop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA("Ronaldo Cagao");
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFODA{
+        private readonly string _NomeJogador;
+
+        public JogoFODA(string nome)
+        {
+            _NomeJogador = nome;
+        }
+        public void IniciarJogo(){
+
+            Console.Write($"{_NomeJogador} Jogador 1 fez passe");
+        }
+
+    }
+
+
+}
